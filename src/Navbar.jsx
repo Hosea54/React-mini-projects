233b9f52361eb1ba10c816a925c8f000c7062b67
Hosea54 <ejohhoseamwongwe@gmail.com>
2025-10-10 22:36:10 -0400

    Added useRef and made link-container dynamic

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,20 +1,35 @@
 import { FaBars } from "react-icons/fa";
 import logo from "./logo.svg";
 import { links, social } from "./data";
+import { useRef, useState } from "react";
 
 const Navbar = () => {
-  const toggleLinks = () => {};
+  const [showLinks, setShowLinks] = useState(false);
+  const linksContainerRef = useRef(null);
+  const linksRef = useRef(null);
+  const toggleLinks = () => {
+    setShowLinks(!showLinks);
+  };
+  const linkStyles = {
+    height: showLinks
+      ? `${linksRef.current.getBoundingClientRect().height}px`
+      : `0px`,
+  };
   return (
     <nav>
       <div className="nav-center">
         <div className="nav-header">
-          <img src={logo} className="nav-header"></img>
+          <img src={logo} className="logo" alt="logo" />
           <button className="nav-toggle" onClick={toggleLinks}>
             <FaBars />
           </button>
         </div>
-        <div className="links-container">
-          <ul className="links">
+        <div
+          className="links-container"
+          ref={linksContainerRef}
+          style={linkStyles}
+        >
+          <ul className="links" ref={linksRef}>
             {links.map((link) => {
               const { id, url, text } = link;
               return (
@@ -26,7 +41,7 @@ const Navbar = () => {
           </ul>
         </div>
         {/* social links */}
-        <ul className="social icons">
+        <ul className="social-icons">
           {social.map((socialIcon) => {
             const { id, url, icon } = socialIcon;
             return (
