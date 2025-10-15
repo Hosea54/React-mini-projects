9b584cae02b0f4aa300516eba8b4ade7fc8d21c3
Hosea54 <ejohhoseamwongwe@gmail.com>
2025-10-15 11:22:10 -0400

    Logo complete

---
+++ src/NavLinks.jsx
@@ -0,0 +1,4 @@
+const NavLinks = () => {
+  return <div className="nav-links">su</div>;
+};
+export default NavLinks;
--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,4 +1,16 @@
+import { FaBars } from "react-icons/fa";
+import NavLinks from "./NavLinks";
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav>
+      <div className="nav-center">
+        <h1 className="logo">Coda</h1>
+        <button className="toggle-btn">
+          <FaBars />
+        </button>
+        <NavLinks />
+      </div>
+    </nav>
+  );
 };
 export default Navbar;
