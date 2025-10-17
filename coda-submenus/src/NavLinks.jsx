872e8daa55191e4a107062aa7d9f5184d82fbed0
Hosea54 <ejohhoseamwongwe@gmail.com>
2025-10-17 18:09:15 -0400

    submenu jsx complete

--- coda-submenus/src/NavLinks.jsx
+++ coda-submenus/src/NavLinks.jsx
@@ -1,11 +1,17 @@
+import { useGlobalContext } from "./context";
 import sublinks from "./data";
 const NavLinks = () => {
+  const { setPageId } = useGlobalContext();
   return (
     <div className="nav-links">
       {sublinks.map((link) => {
         const { pageId, page } = link;
         return (
-          <button key={pageId} className="nav-link">
+          <button
+            key={pageId}
+            className="nav-link"
+            onMouseEnter={() => setPageId(pageId)}
+          >
             {page}
           </button>
         );
--- coda-submenus/src/Submenu.jsx
+++ coda-submenus/src/Submenu.jsx
@@ -2,6 +2,22 @@ import { useGlobalContext } from "./context";
 
 const Submenu = () => {
   const { pageId, setPageId } = useGlobalContext();
-  return <div>Submenu</div>;
+  const currentPage = sublinks.find((item) => item.pageId === pageId);
+  return (
+    <div className="submenu">
+      <h5>{currentPage?.page}</h5>
+      <div className="submenu-links">
+        {currentPage?.links?.map((link) => {
+          const { id, url, label, icon } = link;
+          return (
+            <a key={id} href={url}>
+              {icon}
+              {label}
+            </a>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 export default Submenu;
